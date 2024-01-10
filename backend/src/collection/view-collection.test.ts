23bc9ce72e0439679f05bf048c72164fe0e0d1e0
maxpn01 <cypher.cr@protonmail.com>
2024-01-10 15:03:43 +0400

    fix view collection test

--- backend/src/collection/view-collection.test.ts
+++ backend/src/collection/view-collection.test.ts
@@ -1,80 +1,96 @@
 import { describe, expect, it, beforeEach } from "vitest";
-import {
-	Collection,
-	MemoryCollectionRepository,
-	Topic,
-	MemoryTopicRepository,
-} from ".";
-import { Item, MemoryItemRepository } from "./item";
-import { MemoryUserRepository, User } from "../user";
+import { CollectionRepository } from ".";
+import { ItemRepository } from "./item";
+import { UserRepository } from "../user";
 import { createTestItem } from "./item/index.test";
 import { createTestUser } from "../user/index.test";
 import { createTestCollection, createTestTopic } from "./index.test";
 import { ViewCollectionUseCase } from "./view-collection";
+import { mock, instance, when, deepEqual } from "ts-mockito";
+import { Ok } from "ts-results";
 
 describe("view collection use case", () => {
-	let users: User[];
-	let userRepository: MemoryUserRepository;
-
-	let topics: Topic[];
-	let topicRepository: MemoryTopicRepository;
-
-	let collections: Collection[];
-	let collectionRepository: MemoryCollectionRepository;
-
-	let items: Item[];
-	let itemRepository: MemoryItemRepository;
-
 	let viewCollection: ViewCollectionUseCase;
 
 	beforeEach(() => {
-		topics = [createTestTopic("topic1"), createTestTopic("topic2")];
-		topicRepository = new MemoryTopicRepository(topics);
+		const bookTopic = createTestTopic("books");
 
-		users = [createTestUser("user1")];
-		userRepository = new MemoryUserRepository(users);
-
-		collections = [
-			createTestCollection("collection1", users[0], topics[0]),
-			createTestCollection("collection2", users[0], topics[1]),
-		];
-		collectionRepository = new MemoryCollectionRepository(
-			collections,
-			userRepository,
-			topicRepository,
+		const john = createTestUser("john");
+		const johnCollection = createTestCollection(
+			"johncollection",
+			john,
+			bookTopic,
 		);
-
-		items = [
-			createTestItem("collection1item1", collections[0]),
-			createTestItem("collection1item2", collections[0]),
-			createTestItem("collection1item3", collections[0]),
-			createTestItem("collection2item1", collections[1]),
+		const johnCollectionItems = [
+			createTestItem("harrypotter", johnCollection),
+			createTestItem("hungergames", johnCollection),
 		];
-		itemRepository = new MemoryItemRepository(items, collectionRepository);
 
-		viewCollection = new ViewCollectionUseCase(
-			collectionRepository,
-			itemRepository,
+		const tyler = createTestUser("tyler");
+		const tylerCollection = createTestCollection(
+			"tylercollection",
+			tyler,
+			bookTopic,
 		);
+		const tylerCollectionItems = [
+			createTestItem("themetamorphosisofprimeintellect", johnCollection),
+			createTestItem("cppfordummies", johnCollection),
+			createTestItem("perfume", johnCollection),
+		];
+
+		const MockUserRepo = mock<UserRepository>();
+		const userRepo = instance(MockUserRepo);
+
+		const MockCollectionRepo = mock<CollectionRepository>();
+		when(
+			MockCollectionRepo.get(
+				"johncollection",
+				deepEqual({ include: { items: true } }),
+			),
+		).thenResolve(
+			Ok({
+				collection: johnCollection,
+				items: johnCollectionItems,
+			}),
+		);
+		when(
+			MockCollectionRepo.get(
+				"tylercollection",
+				deepEqual({ include: { items: true } }),
+			),
+		).thenResolve(
+			Ok({
+				collection: tylerCollection,
+				items: tylerCollectionItems,
+			}),
+		);
+		const collectionRepo = instance(MockCollectionRepo);
+
+		const MockItemRepo = mock<ItemRepository>();
+		const itemRepo = instance(MockItemRepo);
+
+		viewCollection = new ViewCollectionUseCase(collectionRepo, itemRepo);
 	});
 
-	it("returns collection 1 from the repository", async () => {
-		const collectionResult = await viewCollection.execute("collection1");
-		const collection = collectionResult.unwrap();
+	it("returns john's collection from the repository", async () => {
+		const viewCollectionResult = await viewCollection.execute("johncollection");
+		const viewedCollection = viewCollectionResult.unwrap();
 
-		expect(collection.id).toBe("collection1");
-		expect(collection.topic.id).toBe("topic1");
-		expect(collection.items.length).toBe(3);
-		expect(collection.items[1].id).toBe("collection1item2");
+		expect(viewedCollection.id).toBe("johncollection");
+		expect(viewedCollection.topic.id).toBe("books");
+		expect(viewedCollection.items.length).toBe(2);
+		expect(viewedCollection.items[1].id).toBe("hungergames");
 	});
 
-	it("returns collection 2 from the repository", async () => {
-		const collectionResult = await viewCollection.execute("collection2");
-		const collection = collectionResult.unwrap();
+	it("returns tyler's collection from the repository", async () => {
+		const viewCollectionResult = await viewCollection.execute(
+			"tylercollection",
+		);
+		const viewedCollection = viewCollectionResult.unwrap();
 
-		expect(collection.id).toBe("collection2");
-		expect(collection.topic.id).toBe("topic2");
-		expect(collection.items.length).toBe(1);
-		expect(collection.items[0].id).toBe("collection2item1");
+		expect(viewedCollection.id).toBe("tylercollection");
+		expect(viewedCollection.topic.id).toBe("books");
+		expect(viewedCollection.items.length).toBe(3);
+		expect(viewedCollection.items[2].id).toBe("perfume");
 	});
 });
