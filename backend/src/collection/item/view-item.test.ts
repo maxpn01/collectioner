28d89dd218155b708f50bde8ffec7ff6e27cfe73
maxpn01 <cypher.cr@protonmail.com>
2024-01-12 22:04:02 +0400

    change view item test

--- backend/src/collection/item/view-item.test.ts
+++ backend/src/collection/item/view-item.test.ts
@@ -1,140 +1,127 @@
 import { beforeEach, describe, it, expect } from "vitest";
-import {
-	Item,
-	ItemFieldRepositories,
-	MemoryItemFieldRepository,
-	MemoryItemRepository,
-} from ".";
-import {
-	Collection,
-	CollectionField,
-	CollectionFieldType,
-	MemoryCollectionFieldRepository,
-	MemoryCollectionRepository,
-	MemoryTopicRepository,
-	Topic,
-} from "..";
-import { MemoryUserRepository, User } from "../../user";
-import { Comment, MemoryCommentRepository } from "./comments";
+import { ItemFields, ItemRepository } from ".";
+import { CollectionField, CollectionFieldType } from "..";
 import { createTestComment } from "./comments/index.test";
 import { ViewItemUseCase } from "./view-item";
 import { createTestUser } from "../../user/index.test";
-import {
-	createTestCollection,
-	createTestCollectionField,
-	createTestTopic,
-} from "../index.test";
+import { createTestCollection, createTestTopic } from "../index.test";
 import { createTestItem } from "./index.test";
+import { instance, mock, when, deepEqual } from "ts-mockito";
+import { Ok } from "ts-results";
 
 describe("view item use case", () => {
 	let viewItem: ViewItemUseCase;
 
-	let users: User[];
-	let userRepository: MemoryUserRepository;
+	const MockItemRepo = mock<ItemRepository>();
 
-	let topics: Topic[];
-	let topicRepository: MemoryTopicRepository;
-
-	let collections: Collection[];
-	let collectionRepository: MemoryCollectionRepository;
-
-	let collectionFields: CollectionField[];
-	let collectionFieldRepository: MemoryCollectionFieldRepository;
-
-	let items: Item[];
-	let itemRepository: MemoryItemRepository;
-
-	let numberFields: Map<string, number>;
-	let numberFieldRepository: MemoryItemFieldRepository<number>;
-
-	let textFields: Map<string, string>;
-	let textFieldRepository: MemoryItemFieldRepository<string>;
-
-	let multilineTextFields: Map<string, string>;
-	let multilineTextFieldRepository: MemoryItemFieldRepository<string>;
-
-	let checkboxFields: Map<string, boolean>;
-	let checkboxFieldRepository: MemoryItemFieldRepository<boolean>;
-
-	let dateFields: Map<string, Date>;
-	let dateFieldRepository: MemoryItemFieldRepository<Date>;
-
-	let comments: Comment[];
-	let commentRepository: MemoryCommentRepository;
+	const john = createTestUser("john");
+	const johnCollection = createTestCollection(
+		"johncollection",
+		john,
+		createTestTopic("books"),
+	);
+	const johnItem = createTestItem("johnitem", johnCollection);
+	johnItem.name = "Harry Potter";
+	const johnComment = createTestComment("johncomment", john.id, johnItem);
+	johnComment.text = "I love it!";
 
 	beforeEach(() => {
-		users = [createTestUser("alice"), createTestUser("john")];
-		userRepository = new MemoryUserRepository(users);
+		const itemRepo = instance(MockItemRepo);
 
-		topics = [createTestTopic("book")];
-		topicRepository = new MemoryTopicRepository(topics);
-
-		collections = [createTestCollection("top50fantasy", users[0], topics[0])];
-		collectionRepository = new MemoryCollectionRepository(
-			collections,
-			userRepository,
-			topicRepository,
-		);
-
-		collectionFields = [
-			createTestCollectionField(
-				"name",
-				CollectionFieldType.Text,
-				collections[0],
-			),
-		];
-		collectionFieldRepository = new MemoryCollectionFieldRepository(
-			collectionFields,
-			collectionRepository,
-		);
-
-		items = [createTestItem("hungergames", collections[0])];
-		itemRepository = new MemoryItemRepository(items, collectionRepository);
-
-		numberFields = new Map();
-		numberFieldRepository = new MemoryItemFieldRepository<number>(numberFields);
-
-		textFields = new Map([["hungergames->name", "Hunger Games"]]);
-		textFieldRepository = new MemoryItemFieldRepository<string>(textFields);
-
-		multilineTextFields = new Map();
-		multilineTextFieldRepository = new MemoryItemFieldRepository<string>(
-			multilineTextFields,
-		);
-
-		checkboxFields = new Map();
-		checkboxFieldRepository = new MemoryItemFieldRepository<boolean>(
-			checkboxFields,
-		);
-
-		dateFields = new Map();
-		dateFieldRepository = new MemoryItemFieldRepository<Date>(dateFields);
-
-		const itemFieldRepositories: ItemFieldRepositories = {
-			number: numberFieldRepository,
-			text: textFieldRepository,
-			multilineText: multilineTextFieldRepository,
-			checkbox: checkboxFieldRepository,
-			date: dateFieldRepository,
-		};
-
-		comments = [createTestComment("comment1", "alice", items[0])];
-		commentRepository = new MemoryCommentRepository(comments, itemRepository);
-
-		viewItem = new ViewItemUseCase(
-			collectionFieldRepository,
-			itemRepository,
-			commentRepository,
-		);
+		viewItem = new ViewItemUseCase(itemRepo);
 	});
 
-	it("returns item1 from the repository", async () => {
-		const itemResult = await viewItem.execute("hungergames");
-		const item = itemResult.unwrap();
+	it("returns item from the repository", async () => {
+		const collectionFields: CollectionField[] = [
+			{
+				id: "pages",
+				name: "Pages",
+				type: CollectionFieldType.Number,
+				collection: johnCollection,
+			},
+			{
+				id: "author",
+				name: "Author",
+				type: CollectionFieldType.Text,
+				collection: johnCollection,
+			},
+			{
+				id: "description",
+				name: "Description",
+				type: CollectionFieldType.MultilineText,
+				collection: johnCollection,
+			},
+			{
+				id: "read",
+				name: "Read",
+				type: CollectionFieldType.Checkbox,
+				collection: johnCollection,
+			},
+			{
+				id: "published",
+				name: "Published",
+				type: CollectionFieldType.Date,
+				collection: johnCollection,
+			},
+		];
 
-		expect(item.id).toBe("hungergames");
-		expect(item.fields.textFields).toEqual(textFields);
-		expect(item.comments[0].id).toBe("comment1");
-		expect(item.comments[0].author).toEqual(users[0]);
+		const itemFields: ItemFields = {
+			numberFields: [
+				{
+					item: johnItem,
+					collectionField: collectionFields[0],
+					value: 333,
+				},
+			],
+			textFields: [
+				{
+					item: johnItem,
+					collectionField: collectionFields[1],
+					value: "JK Rowling",
+				},
+			],
+			multilineTextFields: [
+				{
+					item: johnItem,
+					collectionField: collectionFields[2],
+					value: `Turning the envelope over, his hand trembling, Harry saw a purple wax seal bearing a coat of arms; a lion, an eagle, a badger and a snake surrounding a large letter 'H'.`,
+				},
+			],
+			checkboxFields: [
+				{
+					item: johnItem,
+					collectionField: collectionFields[3],
+					value: true,
+				},
+			],
+			dateFields: [
+				{
+					item: johnItem,
+					collectionField: collectionFields[4],
+					value: new Date("June 26, 1997"),
+				},
+			],
+		};
+
+		when(
+			MockItemRepo.get(
+				johnItem.id,
+				deepEqual({ include: { fields: true, comments: true } }),
+			),
+		).thenResolve(
+			Ok({
+				item: johnItem,
+				fields: itemFields,
+				comments: [johnComment],
+			}),
+		);
+
+		const viewItemResult = await viewItem.execute(johnItem.id);
+		const viewedItem = viewItemResult.unwrap();
+
+		expect(viewedItem.id).toBe(johnItem.id);
+		expect(viewedItem.name).toBe("Harry Potter");
+		expect(viewedItem.fields.textFields.size).toBe(1);
+		expect(viewedItem.comments[0].text).toBe(johnComment.text);
 	});
 });
