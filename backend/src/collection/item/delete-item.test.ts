b298c13c13d9163661b34c84bb43ab50a93391c3
maxpn01 <cypher.cr@protonmail.com>
2024-01-13 14:21:54 +0400

    change delete item test

--- backend/src/collection/item/delete-item.test.ts
+++ backend/src/collection/item/delete-item.test.ts
@@ -1,13 +1,14 @@
-import { describe, beforeEach, it } from "vitest";
+import { describe, beforeEach, it, expect } from "vitest";
 import { ItemRepository } from ".";
 import { CollectionRepository } from "..";
 import { createTestUser } from "../../user/index.test";
 import { createTestTopic, createTestCollection } from "../index.test";
 import { DeleteItemUseCase } from "./delete-item";
 import { UserRepository } from "../../user";
-import { instance, mock, when, verify } from "ts-mockito";
+import { instance, mock, when, verify, resetCalls, anything } from "ts-mockito";
 import { createTestItem } from "./index.test";
 import { None, Ok } from "ts-results";
+import { NotAuthorizedFailure } from "../../user/view-user";
 
 describe("delete item use case", () => {
 	let deleteItem: DeleteItemUseCase;
@@ -22,26 +23,58 @@ describe("delete item use case", () => {
 		john,
 		createTestTopic("books"),
 	);
+	const johnItem = createTestItem("johnitem", johnCollection);
 
-	const item = createTestItem("johncollectionitem", johnCollection);
+	const tyler = createTestUser("tyler");
+	const admin = createTestUser("admin");
+	admin.isAdmin = true;
 
 	beforeEach(() => {
+		resetCalls(MockUserRepo);
 		const userRepo = instance(MockUserRepo);
+
+		resetCalls(MockCollectionRepo);
 		const collectionRepo = instance(MockCollectionRepo);
+
+		resetCalls(MockItemRepo);
 		const itemRepo = instance(MockItemRepo);
+
+		when(MockItemRepo.get(johnItem.id)).thenResolve(Ok({ item: johnItem }));
 
 		deleteItem = new DeleteItemUseCase(userRepo, itemRepo, collectionRepo);
 	});
 
 	it("deletes the item", async () => {
-		const deleteStub = MockItemRepo.delete(item.id);
+		const deleteStub = MockItemRepo.delete(johnItem.id);
 
-		when(MockItemRepo.get("johncollectionitem")).thenResolve(Ok({ item }));
 		when(deleteStub).thenResolve(Ok(None));
 
-		const result = await deleteItem.execute("johncollectionitem", "john");
+		const result = await deleteItem.execute(johnItem.id, john.id);
 		if (result.err) throw result;
 
 		verify(deleteStub).once();
+	});
+
+	it("allow admins delete items", async () => {
+		const deleteStub = MockItemRepo.delete(johnItem.id);
+
+		when(MockUserRepo.get(admin.id)).thenResolve(Ok({ user: admin }));
+		when(deleteStub).thenResolve(Ok(None));
+
+		const result = await deleteItem.execute(johnItem.id, admin.id);
+		if (result.err) throw result;
+
+		verify(deleteStub).once();
+	});
+
+	it("doesn't allow other users delete items", async () => {
+		when(MockUserRepo.get(tyler.id)).thenResolve(Ok({ user: tyler }));
+
+		const result = await deleteItem.execute(johnItem.id, tyler.id);
+		if (result.ok) throw result;
+		const failure = result.val;
+
+		verify(MockItemRepo.delete(anything())).never();
+		expect(failure).toBeInstanceOf(NotAuthorizedFailure);
 	});
 });
