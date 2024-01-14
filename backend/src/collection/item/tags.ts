c534740b184cb55559ca80082c19f5cf2aacd14a
maxpn01 <cypher.cr@protonmail.com>
2024-01-14 14:58:57 +0400

    add tags prisma repository

--- backend/src/collection/item/tags.ts
+++ backend/src/collection/item/tags.ts
@@ -1,9 +1,63 @@
 import { Result, Ok } from "ts-results";
 import { Failure } from "../../utils/failure";
+import { PrismaClient } from "@prisma/client";
 
 export interface TagsRepository {
 	getAll(): Promise<Result<Set<string>, Failure>>;
 	getTagsThatStartWith(s: string): Promise<Result<Set<string>, Failure>>;
+}
+
+class PrismaTagsRepository implements TagsRepository {
+	private prisma: PrismaClient;
+
+	constructor() {
+		this.prisma = new PrismaClient();
+	}
+
+	async getAll(): Promise<Result<Set<string>, Failure>> {
+		const prismaItems = await this.prisma.item.findMany({
+			select: {
+				tags: true,
+			},
+		});
+
+		const tags = new Set<string>();
+
+		prismaItems.forEach((item) => {
+			item.tags.forEach((itemTag) => {
+				tags.add(itemTag.tag);
+			});
+		});
+
+		return Ok(tags);
+	}
+
+	async getTagsThatStartWith(s: string): Promise<Result<Set<string>, Failure>> {
+		const prismaItems = await this.prisma.item.findMany({
+			where: {
+				tags: {
+					some: {
+						tag: {
+							startsWith: s,
+						},
+					},
+				},
+			},
+			include: {
+				tags: true,
+			},
+		});
+
+		const tags = new Set<string>();
+
+		prismaItems.forEach((item) => {
+			item.tags.forEach((itemTag) => {
+				tags.add(itemTag.tag);
+			});
+		});
+
+		return Ok(tags);
+	}
 }
 
 export class AutocompleteTagsUseCase {
