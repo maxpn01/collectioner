fe2958859545a8a45a4a800fc3243a236cdc2277
maxpn01 <cypher.cr@protonmail.com>
2024-01-14 18:13:59 +0400

    change update collection test

--- backend/src/collection/update-collection.test.ts
+++ backend/src/collection/update-collection.test.ts
@@ -1,8 +1,6 @@
 import { describe, expect, it, beforeEach } from "vitest";
 import { Some, None, Ok } from "ts-results";
 import { UserRepository } from "../user";
-import { SetCollectionImageUseCase } from "./update-collection";
-import { NotAuthorizedFailure } from "../user/view-user";
 import { createTestUser } from "../user/index.test";
 import { createTestCollection, createTestTopic } from "./index.test";
 import {
@@ -15,136 +13,324 @@ import {
 	anything,
 } from "ts-mockito";
 import { CollectionRepository } from "./repositories/collection";
+import {
+	UpdateCollectionRequest,
+	UpdateCollectionRequestField,
+	UpdateCollectionRequestNewField,
+	UpdateCollectionUseCase,
+} from "./update-collection";
+import { TopicRepository } from "./repositories/topic";
+import {
+	CollectionFieldRepository,
+	UpdatedField,
+} from "./repositories/collection-field";
+import { Collection, CollectionField, CollectionFieldType } from ".";
+import { NotAuthorizedFailure } from "../user/view-user";
 
-describe("set collection image use case", () => {
-	let setCollectionImage: SetCollectionImageUseCase;
+describe("update collection use case", () => {
+	let updateCollection: UpdateCollectionUseCase;
 
+	const MockTopicRepo = mock<TopicRepository>();
 	const MockUserRepo = mock<UserRepository>();
 	const MockCollectionRepo = mock<CollectionRepository>();
+	const MockCollectionFieldRepo = mock<CollectionFieldRepository>();
+
+	const bookTopic = createTestTopic("books");
+
+	const john = createTestUser("john");
+	const johnCollection = createTestCollection(
+		"johncollection",
+		john,
+		bookTopic,
+	);
+
+	const tyler = createTestUser("tyler");
+	const tylerCollection = createTestCollection(
+		"tylercollection",
+		tyler,
+		bookTopic,
+	);
+
+	const admin = createTestUser("admin");
+	admin.isAdmin = true;
+
+	const updatedCollection: Collection = {
+		owner: john,
+		id: johnCollection.id,
+		name: "top50fantasy",
+		topic: bookTopic,
+		imageOption: Some("image"),
+	};
+
+	const numberField: UpdateCollectionRequestField = {
+		id: "pages",
+		name: "Pages",
+		type: CollectionFieldType.Number,
+	};
+	const textField: UpdateCollectionRequestField = {
+		id: "author",
+		name: "Author",
+		type: CollectionFieldType.Text,
+	};
+	const multilineTextField: UpdateCollectionRequestField = {
+		id: "description",
+		name: "Description",
+		type: CollectionFieldType.MultilineText,
+	};
+	const checkboxField: UpdateCollectionRequestField = {
+		id: "read",
+		name: "Read",
+		type: CollectionFieldType.Checkbox,
+	};
+	const dateField: UpdateCollectionRequestField = {
+		id: "published",
+		name: "Published",
+		type: CollectionFieldType.Date,
+	};
+
+	const collectionFieldsRequest: UpdateCollectionRequestField[] = [
+		numberField,
+		textField,
+		multilineTextField,
+		checkboxField,
+		dateField,
+	];
+
+	const newNumberField: UpdateCollectionRequestNewField = {
+		name: "Pages",
+		type: CollectionFieldType.Number,
+	};
+	const newTextField: UpdateCollectionRequestNewField = {
+		name: "Author",
+		type: CollectionFieldType.Text,
+	};
+	const newMultilineTextField: UpdateCollectionRequestNewField = {
+		name: "Description",
+		type: CollectionFieldType.MultilineText,
+	};
+	const newCheckboxField: UpdateCollectionRequestNewField = {
+		name: "Read",
+		type: CollectionFieldType.Checkbox,
+	};
+	const newDateField: UpdateCollectionRequestNewField = {
+		name: "Published",
+		type: CollectionFieldType.Date,
+	};
+
+	const newCollectionFieldsRequest: UpdateCollectionRequestNewField[] = [
+		newNumberField,
+		newTextField,
+		newMultilineTextField,
+		newCheckboxField,
+		newDateField,
+	];
+
+	const updatedCollectionRequest: UpdateCollectionRequest = {
+		name: updatedCollection.name,
+		topicId: updatedCollection.topic.id,
+		imageOption: updatedCollection.imageOption,
+		fields: collectionFieldsRequest,
+		newFields: newCollectionFieldsRequest,
+	};
+
+	const collectionNumberField: CollectionField = {
+		id: numberField.id,
+		name: newNumberField.name,
+		collection: johnCollection,
+		type: numberField.type,
+	};
+	const collectionTextField: CollectionField = {
+		id: textField.id,
+		name: newTextField.name,
+		collection: johnCollection,
+		type: textField.type,
+	};
+	const collectionMultilineTextField: CollectionField = {
+		id: multilineTextField.id,
+		name: newMultilineTextField.name,
+		collection: johnCollection,
+		type: multilineTextField.type,
+	};
+	const collectionCheckboxField: CollectionField = {
+		id: checkboxField.id,
+		name: newCheckboxField.name,
+		collection: johnCollection,
+		type: checkboxField.type,
+	};
+	const collectionDateField: CollectionField = {
+		id: dateField.id,
+		name: newDateField.name,
+		collection: johnCollection,
+		type: dateField.type,
+	};
+
+	const collectionFields: CollectionField[] = [
+		collectionNumberField,
+		collectionTextField,
+		collectionMultilineTextField,
+		collectionCheckboxField,
+		collectionDateField,
+	];
+
+	const updatedNumberField: UpdatedField = {
+		id: collectionNumberField.id,
+		field: collectionNumberField,
+	};
+	const updatedTextField: UpdatedField = {
+		id: collectionTextField.id,
+		field: collectionTextField,
+	};
+	const updatedMultilineTextField: UpdatedField = {
+		id: collectionMultilineTextField.id,
+		field: collectionMultilineTextField,
+	};
+	const updatedCheckboxField: UpdatedField = {
+		id: collectionCheckboxField.id,
+		field: collectionCheckboxField,
+	};
+	const updatedDateField: UpdatedField = {
+		id: collectionDateField.id,
+		field: collectionDateField,
+	};
+
+	const updatedFields: UpdatedField[] = [
+		updatedNumberField,
+		updatedTextField,
+		updatedMultilineTextField,
+		updatedCheckboxField,
+		updatedDateField,
+	];
+
+	const createdNumberField: CollectionField = {
+		id: anything(),
+		collection: johnCollection,
+		name: numberField.name,
+		type: numberField.type,
+	};
+	const createdTextField: CollectionField = {
+		id: anything(),
+		collection: johnCollection,
+		name: textField.name,
+		type: textField.type,
+	};
+	const createdMultilineTextField: CollectionField = {
+		id: anything(),
+		collection: johnCollection,
+		name: multilineTextField.name,
+		type: multilineTextField.type,
+	};
+	const createdCheckboxField: CollectionField = {
+		id: anything(),
+		collection: johnCollection,
+		name: checkboxField.name,
+		type: checkboxField.type,
+	};
+	const createdDateField: CollectionField = {
+		id: anything(),
+		collection: johnCollection,
+		name: dateField.name,
+		type: dateField.type,
+	};
+
+	const createdFields: CollectionField[] = [
+		createdNumberField,
+		createdTextField,
+		createdMultilineTextField,
+		createdCheckboxField,
+		createdDateField,
+	];
 
 	beforeEach(() => {
-		const bookTopic = createTestTopic("books");
-
-		const admin = createTestUser("admin");
-		admin.isAdmin = true;
-
-		const john = createTestUser("john");
-		const johnCollection = createTestCollection(
-			"johncollection",
-			john,
-			bookTopic,
-		);
-
-		const tyler = createTestUser("tyler");
-		const tylerCollection = createTestCollection(
-			"tylercollection",
-			tyler,
-			bookTopic,
-		);
+		resetCalls(MockTopicRepo);
+		when(MockTopicRepo.get(bookTopic.id)).thenResolve(Ok(bookTopic));
+		const topicRepo = instance(MockTopicRepo);
 
 		resetCalls(MockUserRepo);
-		when(MockUserRepo.get("john")).thenResolve(Ok({ user: john }));
-		when(MockUserRepo.get("tyler")).thenResolve(Ok({ user: tyler }));
-		when(MockUserRepo.get("admin")).thenResolve(Ok({ user: admin }));
+		when(MockUserRepo.get(john.id)).thenResolve(Ok({ user: john }));
+		when(MockUserRepo.get(tyler.id)).thenResolve(Ok({ user: tyler }));
+		when(MockUserRepo.get(admin.id)).thenResolve(Ok({ user: admin }));
 		const userRepo = instance(MockUserRepo);
 
 		resetCalls(MockCollectionRepo);
-		when(MockCollectionRepo.get("johncollection")).thenResolve(
-			Ok({ collection: johnCollection }),
-		);
-		when(MockCollectionRepo.get("tylercollection")).thenResolve(
-			Ok({ collection: tylerCollection }),
+		when(
+			MockCollectionRepo.get(
+				johnCollection.id,
+				deepEqual({ include: { fields: true } }),
+			),
+		).thenResolve(Ok({ collection: johnCollection, fields: collectionFields }));
+		when(
+			MockCollectionRepo.get(
+				tylerCollection.id,
+				deepEqual({ include: { fields: true } }),
+			),
+		).thenResolve(
+			Ok({ collection: tylerCollection, fields: collectionFields }),
 		);
 		const collectionRepo = instance(MockCollectionRepo);
 
-		setCollectionImage = new SetCollectionImageUseCase(
+		resetCalls(MockCollectionFieldRepo);
+		when(
+			MockCollectionFieldRepo.updateMany(deepEqual(updatedFields)),
+		).thenResolve(Ok(None));
+		when(
+			MockCollectionFieldRepo.createMany(deepEqual(createdFields)),
+		).thenResolve(Ok(None));
+		const collectionFieldRepo = instance(MockCollectionFieldRepo);
+
+		updateCollection = new UpdateCollectionUseCase(
 			collectionRepo,
+			topicRepo,
 			userRepo,
+			collectionFieldRepo,
 		);
 	});
 
-	it("should allow the owner", async () => {
-		when(
-			MockCollectionRepo.updateImage(
-				"johncollection",
-				deepEqual(Some("image")),
-			),
-		).thenResolve(Ok(None));
-
-		const result = await setCollectionImage.execute(
-			Some("image"),
-			"johncollection",
-			"john",
+	it("updates a collection", async () => {
+		const updateStub = MockCollectionRepo.update(
+			johnCollection.id,
+			deepEqual(updatedCollection),
 		);
-		expect(result.ok).toBe(true);
 
-		verify(
-			MockCollectionRepo.updateImage(
-				"johncollection",
-				deepEqual(Some("image")),
-			),
-		).once();
+		when(updateStub).thenResolve(Ok(None));
+
+		const result = await updateCollection.execute(
+			johnCollection.id,
+			updatedCollectionRequest,
+			john.id,
+		);
+		if (result.err) throw result;
+
+		verify(updateStub).once();
 	});
 
 	it("should allow an admin", async () => {
-		when(
-			MockCollectionRepo.updateImage(
-				"johncollection",
-				deepEqual(Some("image")),
-			),
-		).thenResolve(Ok(None));
-
-		const result = await setCollectionImage.execute(
-			Some("image"),
-			"johncollection",
-			"admin",
+		const updateStub = MockCollectionRepo.update(
+			johnCollection.id,
+			deepEqual(updatedCollection),
 		);
-		expect(result.ok).toBe(true);
 
-		verify(
-			MockCollectionRepo.updateImage(
-				"johncollection",
-				deepEqual(Some("image")),
-			),
-		).once();
+		when(updateStub).thenResolve(Ok(None));
+
+		const result = await updateCollection.execute(
+			johnCollection.id,
+			updatedCollectionRequest,
+			admin.id,
+		);
+		if (result.err) throw result;
+
+		verify(updateStub).once();
 	});
 
 	it("should not allow another user", async () => {
-		const result = await setCollectionImage.execute(
-			Some("image"),
-			"johncollection",
-			"tyler",
+		const result = await updateCollection.execute(
+			johnCollection.id,
+			updatedCollectionRequest,
+			tyler.id,
 		);
-
-		if (result.ok) throw new Error();
+		if (result.ok) throw result;
 		const failure = result.val;
 
-		verify(MockCollectionRepo.updateImage(anything(), anything())).never();
+		verify(MockCollectionRepo.update(anything(), anything())).never();
 		expect(failure).toBeInstanceOf(NotAuthorizedFailure);
-	});
-
-	it("should remove the image", async () => {
-		const bookTopic = createTestTopic("books");
-		const john = createTestUser("john");
-		const johnCollection = createTestCollection(
-			"johncollection",
-			john,
-			bookTopic,
-		);
-		johnCollection.imageOption = Some("image");
-
-		when(MockCollectionRepo.get("johncollection")).thenResolve(
-			Ok({ collection: johnCollection }),
-		);
-
-		when(
-			MockCollectionRepo.updateImage("johncollection", deepEqual(None)),
-		).thenResolve(Ok(None));
-
-		await setCollectionImage.execute(None, "johncollection", "john");
-
-		verify(
-			MockCollectionRepo.updateImage("johncollection", deepEqual(None)),
-		).once();
 	});
 });
