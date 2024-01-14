5127e68b43c64dbef957684ac7a7b5a07491e218
maxpn01 <cypher.cr@protonmail.com>
2024-01-14 14:59:39 +0400

    add comment prisma repository

--- backend/src/collection/item/comments/index.ts
+++ backend/src/collection/item/comments/index.ts
@@ -1,8 +1,9 @@
-import { Result, None } from "ts-results";
-import { Item } from "..";
-import { User } from "../../../user";
-import { Failure } from "../../../utils/failure";
-import { PrismaClient } from "@prisma/client";
+import { Result, None, Err, Ok } from "ts-results";
+import { Item, prismaItemToEntity } from "..";
+import { PrismaUser, User, prismaUserToEntity } from "../../../user";
+import { Failure, NotFoundFailure } from "../../../utils/failure";
+import { PrismaClient, Comment as PrismaComment } from "@prisma/client";
+import { prismaCollectionToEntity } from "../..";
 
 export type Comment = {
 	item: Item;
@@ -22,24 +23,122 @@ export interface CommentRepository {
 }
 
 class PrismaCommentRepository implements CommentRepository {
-	constructor(private prisma: PrismaClient) {}
+	private prisma: PrismaClient;
 
-	get(id: string): Promise<Result<Comment, Failure>> {
-		throw new Error("Method not implemented.");
+	constructor() {
+		this.prisma = new PrismaClient();
 	}
-	getByItem(itemId: string): Promise<Result<Comment[], Failure>> {
-		throw new Error("Method not implemented.");
+
+	async get(id: string): Promise<Result<Comment, Failure>> {
+		const prismaComment = await this.prisma.comment.findUnique({
+			where: { id },
+			include: {
+				item: {
+					include: {
+						collection: {
+							include: {
+								owner: true,
+								topic: true,
+							},
+						},
+						tags: true,
+					},
+				},
+				author: true,
+			},
+		});
+		if (!prismaComment) return Err(new NotFoundFailure());
+
+		const collection = prismaCollectionToEntity(prismaComment.item.collection);
+
+		const comment: Comment = prismaCommentToEntity(
+			prismaComment,
+			prismaItemToEntity(prismaComment.item, collection),
+		);
+
+		return Ok(comment);
 	}
-	create(comment: Comment): Promise<Result<None, Failure>> {
-		throw new Error("Method not implemented.");
+
+	async getByItem(itemId: string): Promise<Result<Comment[], Failure>> {
+		const prismaComments = await this.prisma.comment.findMany({
+			where: { item: { id: itemId } },
+			include: {
+				item: {
+					include: {
+						collection: {
+							include: {
+								owner: true,
+								topic: true,
+							},
+						},
+						tags: true,
+					},
+				},
+				author: true,
+			},
+		});
+
+		const item =
+			prismaComments.length > 0
+				? prismaItemToEntity(
+						prismaComments[0].item,
+						prismaCollectionToEntity(prismaComments[0].item.collection),
+				  )
+				: null;
+		const comments: Comment[] = prismaComments.map((prismaComment) =>
+			prismaCommentToEntity(prismaComment, item!),
+		);
+
+		return Ok(comments);
 	}
-	update(comment: Comment): Promise<Result<None, Failure>> {
-		throw new Error("Method not implemented.");
+
+	async create(comment: Comment): Promise<Result<None, Failure>> {
+		await this.prisma.comment.create({
+			data: {
+				id: comment.id,
+				itemId: comment.item.id,
+				userId: comment.author.id,
+				text: comment.text,
+				createdAt: comment.createdAt,
+			},
+		});
+
+		return Ok(None);
 	}
-	delete(id: string): Promise<Result<None, Failure>> {
-		throw new Error("Method not implemented.");
+
+	async update(comment: Comment): Promise<Result<None, Failure>> {
+		await this.prisma.comment.update({
+			where: { id: comment.id },
+			data: {
+				text: comment.text,
+			},
+		});
+
+		return Ok(None);
 	}
-	deleteByItem(itemId: string): Promise<Result<None, Failure>> {
-		throw new Error("Method not implemented.");
+
+	async delete(id: string): Promise<Result<None, Failure>> {
+		await this.prisma.comment.delete({ where: { id } });
+
+		return Ok(None);
 	}
+
+	async deleteByItem(itemId: string): Promise<Result<None, Failure>> {
+		await this.prisma.comment.deleteMany({ where: { itemId } });
+
+		return Ok(None);
+	}
+}
+
+export function prismaCommentToEntity(
+	model: PrismaComment & { author: PrismaUser },
+	item: Item,
+): Comment {
+	return {
+		item,
+		author: prismaUserToEntity(model.author),
+		id: model.id,
+		text: model.text,
+		createdAt: model.createdAt,
+	};
 }
