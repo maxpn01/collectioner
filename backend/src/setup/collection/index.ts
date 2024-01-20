48c7f1416b22f27176d54b5cf5aef99f24d0c6f4
maxpn01 <cypher.cr@protonmail.com>
2024-01-20 16:32:02 +0400

    add create collection endpoint

--- backend/src/setup/collection/index.ts
+++ backend/src/setup/collection/index.ts
@@ -1,10 +1,30 @@
+import {
+	CreateCollectionUseCase,
+	ExpressCreateCollection,
+} from "../../collection/create-collection";
 import { PrismaCollectionRepository } from "../../collection/repositories/collection";
 import { PrismaTopicRepository } from "../../collection/repositories/topic";
 import { MeiliCollectionSearchEngine } from "../../collection/search-engine";
-import { meili } from "../search";
+import { expressApp } from "../http";
+import { meili } from "../meili";
+import { requireAuth } from "../middleware/auth";
+import { prismaUserRepository } from "../user";
 
 export const prismaCollectionRepository = new PrismaCollectionRepository();
 export const prismaTopicRepository = new PrismaTopicRepository();
 export const meiliCollectionSearchEngine = new MeiliCollectionSearchEngine(
 	meili,
+);
+
+const createCollection = new CreateCollectionUseCase(
+	prismaCollectionRepository,
+	meiliCollectionSearchEngine,
+	prismaTopicRepository,
+	prismaUserRepository,
+);
+const expressCreateCollection = new ExpressCreateCollection(createCollection);
+expressApp.post(
+	"/api/collection",
+	requireAuth,
+	expressCreateCollection.execute,
 );
